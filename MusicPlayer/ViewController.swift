5500fa7dbd7b9fcd238e0a8111c9f9b440324e74
Max Chesnikov <maxches99@gmail.com>
2019-12-07 23:00:58 +0300

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Максим Чесников on 07.12.2019.
+//  Copyright © 2019 Максим Чесников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
